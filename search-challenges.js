7b69fca9c95993e6089c24884a6ad0b47440304b
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-09-06 22:25:50 +0530

    Add find given coding challenge in array functionality

--- search-challenges.js
+++ search-challenges.js
@@ -1,6 +1,16 @@
 const codingChallengesList = document.getElementById('algorithms_session_coding_challenge_id').options;
 
+const codingChallengesArray = [];
+
 for (let i = 0; i < codingChallengesList.length; i+=1) {
-  const codingChallenge = codingChallengesList[i];
-  console.log(codingChallenge.innerText);
+  codingChallengesArray.push(codingChallengesList[i].innerText);
 }
+
+function searchChallenges (query) {
+  for (var j = 0; j < codingChallengesArray.length; j+=1) {
+    if (codingChallengesArray[j].match(query)) return j;
+  }
+  return -1;
+}
+
+console.log(searchChallenges(/Picking Numbers \(HackerRank\)/));
