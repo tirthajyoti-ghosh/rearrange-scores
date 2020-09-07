42451e4806f15a759e18b60821e839b2e38f7e60
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-09-07 16:20:35 +0530

    Create query input field

--- search-challenges.js
+++ search-challenges.js
@@ -1,4 +1,5 @@
-const codingChallengesList = document.getElementById('algorithms_session_coding_challenge_id').options;
+const codingChallenges = document.getElementById('algorithms_session_coding_challenge_id');
+const codingChallengesList = codingChallenges.options;
 
 const codingChallengesArray = [];
 
@@ -17,4 +18,20 @@ function sanitizeQuery(string) {
   return new RegExp(string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'));
 }
 
-codingChallengesList[searchChallenges(sanitizeQuery("Library"))].setAttribute('selected', '');
+codingChallengesList[searchChallenges(sanitizeQuery("Find"))].setAttribute('selected', '');
+
+const createInput = () => {
+  const input = document.createElement('input');
+
+  input.setAttribute('type', 'text');
+
+  input.setAttribute('class', 'form-control mb-3');
+
+  input.setAttribute('placeholder', 'Start typing the name of the coding challenge...');
+  
+  return input;
+};
+
+const container = document.querySelector('#new_algorithms_session > div.bg-white.pt-3.pb-3.without-button > div:nth-child(10)');
+
+container.insertBefore(createInput(), codingChallenges);
